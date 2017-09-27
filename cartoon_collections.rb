1e1b620ada792240b920d0755d5de99367a433b5
Brandon  Harris <cheeridon@gmail.com>
2017-09-27 19:24:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves dwarves
   dwarves.each{|dwarf| puts dwarf}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet calls 
+  calls.map{|calls| calls.capitalize << "!"}
 end
 
 def long_planeteer_calls# code an argument here
