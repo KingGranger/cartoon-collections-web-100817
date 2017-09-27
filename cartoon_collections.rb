2a044de8c39c85bc488333018f53d2f97fa03000
Brandon  Harris <cheeridon@gmail.com>
2017-09-27 19:51:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves dwarves
-  dwarves.each_with_index{|dwarf, number| number dwarf}
+  dwarves.each_with_index{|dwarf, idx| puts "#{idx}.#{dwarf}"}
 end
 
 def summon_captain_planet calls
